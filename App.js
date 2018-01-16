12abebecd0ad27c02b0f6de565423c6200cc12e1
Simon Ayzman <simonayzman@gmail.com>
2018-01-15 19:49:04 -0500

    Added extra line to intro screen

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>LOOK AT ALL THESE AMAZING CHANGES.</Text>
       </View>
     );
   }

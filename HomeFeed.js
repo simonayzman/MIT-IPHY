9e2064b80c40501eed61dc7ddd823eb201f6544e
Simon Ayzman <simonayzman@gmail.com>
2018-01-15 20:19:05 -0500

    Added basic list of cells

--- HomeFeed.js
+++ HomeFeed.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  FlatList,
 } from 'react-native';
 
 export default class HomeFeed extends Component {
+
+  renderItem() {
+    return <View style={styles.cell} />
+  }
+
   render() {
-    return <View style={styles.container} />
+    const gifData = ['sameGifUrl1', 'sameGifUrl2', 'sameGifUrl3', 'sameGifUrl4'];
+    return (
+      <FlatList
+        renderItem={this.renderItem}
+        data={gifData}
+        style={styles.container}
+      />
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 150,
-    width: 150,
+    flex: 1,
     backgroundColor: 'green',
+  },
+  cell: {
+    height: 100,
+    width: 100,
+    margin: 10,
+    backgroundColor: 'red',
   },
 });
